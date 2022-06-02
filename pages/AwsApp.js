d1ce6a2b91f8d7c48c5258da38dc461557384485
Nethma Dissanyake <chamod224.nethma@gmail.com>
2022-06-02 23:48:43 +0530

    AWS_Config_CODE(NETHAM-2)

---
+++ pages/AwsApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AWSImageUpload from "../custom/AWSImageUpload";
+
+function AwsApp() {
+  return (
+    <div>
+      <AWSImageUpload />
+    </div>
+  );
+}
+
+export default AwsApp;
